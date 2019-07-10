e8f1ebdec35091e0ae7dfd89d23749497ea4daa5
Szymon Matuszewski <szymon.matuszewski@pronos.eu>
2019-07-10 09:31:50 +0200

    Init commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_app_service_plan" "app_plan" {
+  name                = var.app_plan_name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  kind                = var.app_kind
+
+  sku {
+    tier = var.sku_tier
+    size = var.sku_size
+  }
+}
+
+resource "azurerm_app_service" "web_app" {
+  count               = length(var.app_names)
+  name                = var.app_names[count.index]
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
+  site_config {
+    always_on                 = true
+    websockets_enabled        = var.websockets_enabled[count.index]
+    use_32_bit_worker_process = var.use_32_bit_worker_process
+  }
+  app_settings = var.app_settings[count.index]
+}
---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of resource group"
+  default     = "web-app-rg"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region where resources will be created"
+  default     = "northeurope"
+}
+
+variable "app_plan_name" {
+  type        = string
+  description = "Name of app service plan to create"
+  default     = "app-plan"
+}
+
+variable "app_kind" {
+  type    = string
+  default = "Windows"
+}
+
+variable "sku_tier" {
+  type    = string
+  default = "Standard"
+}
+
+variable "sku_size" {
+  type    = string
+  default = "S1"
+}
+
+variable "app_names" {
+  type        = list(string)
+  description = "List of app names to create."
+  default     = ["web-app"]
+}
+
+variable "websockets_enabled" {
+  type        = list(bool)
+  description = "Determines if websockets should be enabled for web apps"
+  default     = [false]
+}
+
+variable "use_32_bit_worker_process" {
+  type        = bool
+  description = "When using an App Service Plan in the Free or Shared Tiers use_32_bit_worker_process must be set to true."
+  default     = true
+}
+
+variable "app_settings" {
+  type        = list
+  description = "List of application settings to set on deploy."
+  default     = []
+}
+
+variable "tags" {
+  type        = "map"
+  description = "Map of tags to assign to resources"
+  default     = {}
+}
