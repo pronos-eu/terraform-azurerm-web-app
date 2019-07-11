6f267253b34481e259d9a106019ea0e72775da36
Szymon Matuszewski <22324341+szymon3@users.noreply.github.com>
2019-07-11 14:22:44 +0200

    Add output values

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "web_apps_ids" {
+  value = azurerm_app_service.web_app.*.id
+}
+
+output "web_apps_names" {
+  value = azurerm_app_service.web_app.*.name
+}
+
+output "web_apps_outbound_ips" {
+  value = azurerm_app_service.web_app.*.outbound_ip_addresses
+}
+
+output "web_apps_possible_outbound_ips" {
+  value = azurerm_app_service.web_app.*.possible_outbound_ip_addresses
+}
+
+output "web_apps_hostnames" {
+  value = azurerm_app_service.web_app.*.default_site_hostname
+}
