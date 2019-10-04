4fed9834be34476058a13cece5d367b51b686936
Szymon Matuszewski <22324341+szymon3@users.noreply.github.com>
2019-10-04 09:30:27 +0200

    Remove default values for RG Name and App Plan name vars

--- variables.tf
+++ variables.tf
@@ -1,19 +1,17 @@
 variable "resource_group_name" {
   type        = string
   description = "Name of resource group"
-  default     = "web-app-rg"
+}
+
+variable "app_plan_name" {
+  type        = string
+  description = "Name of app service plan to create"
 }
 
 variable "location" {
   type        = string
   description = "Azure region where resources will be created"
   default     = "northeurope"
-}
-
-variable "app_plan_name" {
-  type        = string
-  description = "Name of app service plan to create"
-  default     = "app-plan"
 }
 
 variable "app_kind" {
@@ -29,6 +27,11 @@ variable "sku_tier" {
 variable "sku_size" {
   type    = string
   default = "S1"
+}
+
+variable "apps" {
+  type        = map(string)
+  description = "Map of application and its settings"
 }
 
 variable "app_names" {
@@ -78,3 +81,4 @@ variable "tags" {
   description = "Map of tags to assign to resources"
   default     = {}
 }
+
