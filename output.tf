2c38af96e7db3569e4c70ca436fa20fbe1148d19
Szymon Matuszewski <22324341+szymon3@users.noreply.github.com>
2019-10-07 11:31:42 +0200

    Change output key from id to name Add web app name to output map

--- output.tf
+++ output.tf
@@ -2,8 +2,9 @@ output "apps" {
   description = "Map of deployed web applications"
   value = {
     for a in azurerm_app_service.main :
-    a.id => {
+    a.name => {
       id                             = a.id
+      name                           = a.name
       default_site_hostname          = a.default_site_hostname
       outbound_ip_addresses          = a.outbound_ip_addresses
       possible_outbound_ip_addresses = a.possible_outbound_ip_addresses
